62869e296f2dbbd6f9bd66ceb0be92404784b9dd
Michael Paulini <mh6@sanger.ac.uk>
2024-05-24 11:17:40 +0100

    added biotypes for ensembl GTFs

--- methods/gtf_stats.rb
+++ methods/gtf_stats.rb
@@ -1,15 +1,19 @@
 #!/bin/env ruby
 # gtf_stats.rb <GTF>
+# zcat <GTF.gz> | ruby gtf_stats.rb
 
 transcripts = 0
 genes       = 0
 exons       = 0
+gene_biotypes = Hash.new(0)
 
 ARGF.each_line{|line|
   columns = line.split
+
   case columns[2]
   when "gene"
     genes += 1
+    gene_biotypes["#{$1}"] +=1 if /gene_biotype\s+"(\w+)"/.match(line)
   when "transcript"
     transcripts += 1
   when "exon"
@@ -22,4 +26,9 @@ genes: #{genes}
 transcripts: #{transcripts}
 transcripts / gene: #{transcripts.to_f/genes}
 exons / transcript: #{exons.to_f/transcripts}
+
 HERE
+
+gene_biotypes.each{|k,v|
+    puts "#{k} => #{v}"
+}
