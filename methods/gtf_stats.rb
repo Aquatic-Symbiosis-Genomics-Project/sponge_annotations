e711e9db6faec04ca2f9e509650592ad564e4dab
Michael Paulini <mh6@sanger.ac.uk>
2025-04-07 11:58:29 +0100

    added length stats

--- methods/gtf_stats.rb
+++ methods/gtf_stats.rb
@@ -6,6 +6,8 @@ transcripts = 0
 genes       = 0
 exons       = 0
 gene_biotypes = Hash.new(0)
+total_gene_length = 0
+total_exon_length = 0
 
 ARGF.each_line{|line|
   columns = line.split
@@ -14,18 +16,23 @@ ARGF.each_line{|line|
   when "gene"
     genes += 1
     gene_biotypes["#{$1}"] +=1 if /gene_biotype\s+"(\w+)"/.match(line)
+    total_gene_length+=(columns[4].to_i-columns[3].to_i+1)
   when "transcript"
     transcripts += 1
   when "exon"
     exons += 1
+    total_exon_length+=(columns[4].to_i-columns[3].to_i+1)
   end
 }
 
 puts <<-HERE
 genes: #{genes}
+average gene length: #{total_gene_length/genes}) bp
 transcripts: #{transcripts}
 transcripts / gene: #{transcripts.to_f/genes}
+average transcript length: #{total_exon_length/transcripts} bp
 exons / transcript: #{exons.to_f/transcripts}
+average exon length: #{total_exon_length/exons} bp
 
 HERE
 
